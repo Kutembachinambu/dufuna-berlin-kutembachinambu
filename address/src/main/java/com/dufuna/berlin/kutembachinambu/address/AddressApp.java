d38642c9b0608b44941cc168a32b5ac303ecc0c6
Kutemba Chinambu <kutembachinambu@rocketmail.com>
2022-06-22 23:23:29 +0200

    Added Hello World App

---
+++ address/src/main/java/com/dufuna/berlin/kutembachinambu/address/AddressApp.java
@@ -0,0 +1,13 @@
+package com.dufuna.berlin.kutembachinambu.address;
+
+/**
+ * Hello world!
+ *
+ */
+public class AddressApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ address/src/test/java/com/dufuna/berlin/kutembachinambu/address/AddressAppTest.java
@@ -0,0 +1,20 @@
+package com.dufuna.berlin.kutembachinambu.address;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AddressAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
